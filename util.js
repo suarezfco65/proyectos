2cbe3ce333be48f217f6e47ce9867e47939b3d1f
pdvsaaitcys <pdvsaaitcys@gmail.com>
2024-11-29 08:35:40 -0400

    mostrar ficha completa

--- util.js
+++ util.js
@@ -5,3 +5,16 @@ const addContentHTML = (id, file) => {
       document.getElementById(id).innerHTML = data;
     });
 }
+function getFieldFromLocation(campo) {
+  // Obtener la URL actual
+  const urlActual = window.location.href;
+  
+  // Crear un objeto URL
+  const url = new URL(urlActual);
+  
+  // Obtener el valor del campo específico
+  const valor = url.searchParams.get(campo);
+  
+  // Retornar el valor o null si no existe
+  return valor ? valor : null;
+}
